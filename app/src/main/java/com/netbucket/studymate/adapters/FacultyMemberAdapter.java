aeaeb681d10cabe39fcca22b7980096d48fcf962
Pushpendra Yadav <pushpendray1337@gmail.com>
2020-12-30 21:49:24 +0530

    Create FacultyMemberAdapter.java

---
+++ app/src/main/java/com/netbucket/studymate/adapters/FacultyMemberAdapter.java
@@ -0,0 +1,185 @@
+package com.netbucket.studymate.adapters;
+
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.netbucket.studymate.R;
+import com.netbucket.studymate.activities.ViewUserProfileActivity;
+import com.netbucket.studymate.model.FacultyMember;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+import es.dmoral.toasty.Toasty;
+
+public class FacultyMemberAdapter extends FirestoreRecyclerAdapter<FacultyMember, FacultyMemberAdapter.FacultyMemberHolder> {
+    private final FirebaseFirestore mStore = FirebaseFirestore.getInstance();
+
+    public FacultyMemberAdapter(@NonNull FirestoreRecyclerOptions<FacultyMember> options) {
+        super(options);
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull final FacultyMemberHolder holder, final int position, @NonNull final FacultyMember model) {
+
+        holder.textViewName.setText(model.getFullName());
+        holder.textViewFacultyId.setText(model.getId());
+        holder.textViewCourse.setText(model.getCourse());
+
+        Glide
+                .with(holder.imageViewProfileImage.getContext())
+                .load(model.getProfileImageUri())
+                .placeholder(R.drawable.ic_outline_location_city_24)
+                .into(holder.imageViewProfileImage);
+
+        holder.constraintLayoutItem.setOnClickListener(v -> {
+            BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(holder.constraintLayoutItem.getContext());
+            View bottomSheetView = LayoutInflater.from(holder.constraintLayoutItem.getContext()).inflate(R.layout.bottom_sheet_member_options, holder.itemView.findViewById(R.id.linearLayout_bottom_sheet_container));
+
+            LinearLayout grantEditProfileAccessLayout = bottomSheetView.findViewById(R.id.linearLayout_grant_profile_edit_access);
+            LinearLayout revokeEditProfileAccessLayout = bottomSheetView.findViewById(R.id.linearLayout_revoke_profile_edit_access);
+
+            if (model.getProfileEditAccess().equals("granted")) {
+                grantEditProfileAccessLayout.setVisibility(View.GONE);
+                revokeEditProfileAccessLayout.setVisibility(View.VISIBLE);
+            }
+
+            bottomSheetView.findViewById(R.id.linearLayout_view_profile).setOnClickListener(v1 -> {
+                Intent intent1 = new Intent(v1.getContext(), ViewUserProfileActivity.class);
+                intent1.putExtra("fullName", model.getFullName());
+                intent1.putExtra("email", model.getEmail());
+                intent1.putExtra("phoneNumber", model.getPhoneNumber());
+                intent1.putExtra("username", model.getUsername());
+                intent1.putExtra("about", model.getAbout());
+                intent1.putExtra("birthday", model.getBirthday());
+                intent1.putExtra("gender", model.getGender());
+                intent1.putExtra("institute", model.getInstitute());
+                intent1.putExtra("role", model.getRole());
+                intent1.putExtra("course", model.getCourse());
+                intent1.putExtra("id", model.getId());
+                intent1.putExtra("semOrYear", model.getSemOrYear());
+                intent1.putExtra("profileImageUri", model.getProfileImageUri());
+                intent1.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+                bottomSheetDialog.dismiss();
+
+                v1.getContext().startActivity(intent1);
+            });
+
+            bottomSheetView.findViewById(R.id.linearLayout_grant_profile_edit_access).setOnClickListener(v12 -> {
+                Map<String, Object> profileEditAccess = new HashMap<>();
+                profileEditAccess.put("profileEditAccess", "granted");
+
+                bottomSheetDialog.dismiss();
+
+                getSnapshots()
+                        .getSnapshot(holder.getAdapterPosition())
+                        .getReference()
+                        .update(profileEditAccess);
+
+                Toasty.success(v.getContext(), "Profile edit access granted!", Toast.LENGTH_SHORT, true).show();
+
+            });
+
+            bottomSheetView.findViewById(R.id.linearLayout_revoke_profile_edit_access).setOnClickListener(v12 -> {
+                Map<String, Object> editAccess = new HashMap<>();
+                editAccess.put("editAccess", "revoked");
+
+                bottomSheetDialog.dismiss();
+
+                getSnapshots()
+                        .getSnapshot(holder.getAdapterPosition())
+                        .getReference()
+                        .update(editAccess);
+
+                Toasty.success(v.getContext(), "Profile edit access revoked!", Toast.LENGTH_SHORT, true).show();
+
+            });
+
+            bottomSheetView.findViewById(R.id.linearLayout_suspend_member).setOnClickListener(v13 -> {
+                Map<String, Object> userStatus = new HashMap<>();
+                userStatus.put("userStatus", "suspended");
+
+                bottomSheetDialog.dismiss();
+
+                mStore.collection("users")
+                        .document(getSnapshots().getSnapshot(holder.getAdapterPosition()).getId())
+                        .update(userStatus)
+                        .addOnSuccessListener(aVoid -> {
+
+                            getSnapshots()
+                                    .getSnapshot(holder.getAdapterPosition())
+                                    .getReference()
+                                    .update(userStatus);
+
+                            Toasty.success(v.getContext(), model.getFullName() + " suspended!", Toast.LENGTH_SHORT, true).show();
+                        })
+                        .addOnFailureListener(e -> Toasty.error(v.getContext(), "Failed to suspend " + model.getFullName() + ", Try again!", Toast.LENGTH_SHORT, true).show());
+            });
+
+            bottomSheetView.findViewById(R.id.linearLayout_delete_member).setOnClickListener(v14 -> {
+                Map<String, Object> userStatus = new HashMap<>();
+                userStatus.put("userStatus", "disallowed");
+
+                bottomSheetDialog.dismiss();
+
+                mStore.collection("users")
+                        .document(getSnapshots().getSnapshot(holder.getAdapterPosition()).getId())
+                        .update(userStatus)
+                        .addOnSuccessListener(aVoid -> {
+
+                            getSnapshots()
+                                    .getSnapshot(holder.getAdapterPosition())
+                                    .getReference()
+                                    .update(userStatus);
+
+                            Toasty.success(v.getContext(), model.getFullName() + " blocked!", Toast.LENGTH_SHORT, true).show();
+                        })
+                        .addOnFailureListener(e -> Toasty.error(v.getContext(), "Failed to block " + model.getFullName() + ", Try again!", Toast.LENGTH_SHORT, true).show());
+            });
+            bottomSheetDialog.setContentView(bottomSheetView);
+            bottomSheetDialog.show();
+        });
+    }
+
+    @NonNull
+    @Override
+    public FacultyMemberHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_faculty_member, parent, false);
+        return new FacultyMemberHolder(view);
+    }
+
+    static class FacultyMemberHolder extends RecyclerView.ViewHolder {
+        TextView textViewName;
+        TextView textViewFacultyId;
+        TextView textViewCourse;
+        CircleImageView imageViewProfileImage;
+        ConstraintLayout constraintLayoutItem;
+
+        public FacultyMemberHolder(View itemView) {
+            super(itemView);
+            textViewName = itemView.findViewById(R.id.textView_full_name);
+            textViewFacultyId = itemView.findViewById(R.id.textView_faculty_id);
+            textViewCourse = itemView.findViewById(R.id.textView_course);
+            imageViewProfileImage = itemView.findViewById(R.id.imageView_profile_image);
+            constraintLayoutItem = itemView.findViewById(R.id.constraintLayout_item);
+        }
+    }
+}
