45a6c5b560a7dcc620dccdf26a7a35dcccafb8a2
Pushpendra Yadav <pushpendray1337@gmail.com>
2020-12-30 21:49:09 +0530

    Create FacultyMemberNoticesActivity.java

---
+++ app/src/main/java/com/netbucket/studymate/activities/FacultyMemberNoticesActivity.java
@@ -0,0 +1,16 @@
+package com.netbucket.studymate.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.netbucket.studymate.R;
+
+public class FacultyMemberNoticesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_faculty_member_notices);
+    }
+}
