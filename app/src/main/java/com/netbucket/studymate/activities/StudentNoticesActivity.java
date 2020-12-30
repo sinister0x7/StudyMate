abd21425b9dc032a691382961b00a8ce83bc3869
Pushpendra Yadav <pushpendray1337@gmail.com>
2020-12-30 21:49:19 +0530

    Create StudentNoticesActivity.java

---
+++ app/src/main/java/com/netbucket/studymate/activities/StudentNoticesActivity.java
@@ -0,0 +1,16 @@
+package com.netbucket.studymate.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.netbucket.studymate.R;
+
+public class StudentNoticesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_notices);
+    }
+}
